c5550ce24229269c955cac5183fa3156d925489d
ryan7falcon <ryan7falcon@gmail.com>
2016-10-20 15:54:52 -0400

    started auth

---
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const AuthenticateContainer = React.createClass({
+  render () {
+    return (
+      <Authenticate />
+    )
+  },
+})
+export default AuthenticateContainer
